66eafaa41b6cb0c1e7d1dea2addcf2c761e4b443
Denis Zarubin <den99f@me.com>
2021-10-22 00:10:08 +0300

    some progress

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
 """Main file to execute the runner."""
+import os
 
-import rpa
+from controllers.pagescrapper import AgencyParser, InvestmentParser, browser_open_page, ItDashboardParserMixin
+from controllers.aggreagator import merge_data_to_xls
+from multiprocessing.pool import Pool
 
 
-rpa.init()
-rpa.url('https://www.google.com')
-rpa.type('//*[@name="q"]', 'decentralization[enter]')
-print(r.read('result-stats'))
-rpa.snap('page', 'results.png')
-rpa.close()
+def x(a):
+    return a
+
+
+def run():
+    """
+    Default runner for parsing data
+    """
+
+    output = os.environ.get("RESULTS_PATH", "output").strip('/')
+    path = f"{os.getcwd()}/{output}"
+
+    # agency_data = AgencyParser(
+    #     parent_xpath="//div[@id='agency-tiles-container']//div[@class='tuck-5']/div[@class='row top-gutter-20']",
+    #     results_path=path,
+    # )
+    investment_data = InvestmentParser(
+        agency_title="Department of Commerce",
+        parent_xpath="//div[@id='agency-tiles-container']//div[@class='tuck-5']/div[@class='row top-gutter-20']",
+        results_path=path,
+
+    )
+    with Pool(10) as p:
+
+        p.map(ItDashboardParserMixin().browser_open_page, investment_data.links)
+
+    # merge_data_to_xls(agency_data, investment_data)
+
+
+if __name__ == "__main__":
+    run()
